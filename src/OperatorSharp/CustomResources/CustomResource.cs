1b7d71abd153cb4a88964eaef29a301b5739405d
Eric Montgomery <ericm@remphoto.net>
2019-10-26 15:16:52 -0600

    Initial operator setup

---
+++ src/OperatorSharp/CustomResources/CustomResource.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using k8s;
+using k8s.Models;
+
+namespace OperatorSharp.CustomResources
+{
+    // Adapted from https://github.com/engineerd/kubecontroller-csharp
+    public abstract class CustomResource : KubernetesObject 
+    {
+        public V1ObjectMeta Metadata { get; set; }
+
+        public T GetAttribute<T>() where T: Attribute {
+            T attribute = Attribute.GetCustomAttribute(this.GetType(), typeof(T)) as T;
+
+            return attribute;
+        }
+    }
+
+    public abstract class CustomResource<TSpec, TStatus> : CustomResource {
+        public TSpec Spec { get; set; }
+        public TStatus Status { get; set; }
+    }
+}
