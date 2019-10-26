2b33280fef64219aa4ecbf37dccc8cd7b6cc19fa
Eric Montgomery <ericm@remphoto.net>
2019-10-26 15:16:25 -0600

    Initial project creation

---
+++ src/mssql-operator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MSSqlOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
